5fe899b7fcaccedbecf6f4daa7a7b9e0cf5ac3be
Jens-k-m-m <jensmonteiro@gmail.com>
2022-05-30 11:52:23 +0200

    PostMan vikra createRider

--- src/main/java/com/example/backend/controller/RiderController.java
+++ src/main/java/com/example/backend/controller/RiderController.java
@@ -1,14 +1,32 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.Rider;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface RiderController {
-
-  @GetMapping("/riders")
+  /**
+   * Get a list of Riders from the database. The mapping is /show/players
+   * @return A list of players
+   *  @author Jens
+   */
+  @GetMapping("/show/riders")
   public List<Rider> getAllRiders();
+
+  /**
+   * Create a rider in the database. When done it returns a HTTP response 200 (OK) Mapping is
+   * /create/rider
+   *
+   * @param rider - given player to create.
+   * @return A HTTP response
+   * @author Jens
+   */
+  @PostMapping("/create/riders")
+  public ResponseEntity<Rider> createRider(@RequestBody Rider rider);
 }
 
 
--- src/main/java/com/example/backend/controller/RiderControllerImpl.java
+++ src/main/java/com/example/backend/controller/RiderControllerImpl.java
@@ -3,18 +3,29 @@ package com.example.backend.controller;
 import com.example.backend.model.Rider;
 import com.example.backend.repository.RiderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @CrossOrigin
 @RestController
 public class RiderControllerImpl implements RiderController {
 
   @Autowired
   RiderRepository riderRepository;
+
   @Override
-  public List<Rider>getAllRiders() {
+  public List<Rider> getAllRiders() {
     return riderRepository.findAll();
+  }
+
+
+  @Override
+  public ResponseEntity<Rider> createRider(Rider rider) {
+    riderRepository.save(rider);
+    return new ResponseEntity<>(rider, HttpStatus.CREATED);
   }
 }
